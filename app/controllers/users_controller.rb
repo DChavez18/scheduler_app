623e548503b1b13d6d590145fecfa4bb8c8e6d40
Derek Chavez <frrenose17@yahoo.com>
2025-02-28 10:03:49 -0700

    feat: add new method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
