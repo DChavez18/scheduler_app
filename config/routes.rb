533b485b0ccfcc1beab340303b696a9fbc40bd63
Derek Chavez <frrenose17@yahoo.com>
2025-02-28 10:15:42 -0700

    feat: add routes for user: edit/update

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :users, only: [:index, :show, :create, :new]
+  resources :users, only: [:index, :show, :create, :new, :edit, :update, :destroy]
+  
 end
